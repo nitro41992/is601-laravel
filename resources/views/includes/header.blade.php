b2cdf70c3039f6d636cbbfd741c6fd8dcc155d1d
kuchiman <kuchiman@njit.edu>
2019-03-23 03:29:38 -0400

    Fix: Minor Text and Header Updates

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,7 +1,7 @@
 
 <nav class="navbar navbar-expand-sm navbar-dark bg-primary">
     <div class="container">
-        <a class="navbar-brand" href="/">Learana</a>
+        <a class="navbar-brand" href="/">is601-Laravel</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
                 aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
